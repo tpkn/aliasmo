7056b6081bddba4b636a4d653a0d32b5813fcaa7
tpkn <tpkn@users.noreply.github.com>
2019-04-23 21:39:20 +0300

    Add files via upload

--- README.md
+++ README.md
@@ -1,2 +1,37 @@
-# aliasmo
+# Aliasmo
 Require NodeJs module using alias
+
+:warning: Tested only on CommonJS system, and not too far in deep.
+
+
+## API
+```javascript
+require('aliasmo')();
+
+// or 
+
+require('aliasmo')({ 
+   "@root": ".",
+   "@config": "./config.js",
+   "@routes": "routes/",
+   "@util": "modules/util"
+});
+```
+
+## package.json
+```json
+{
+   "name": "app",
+   "version": "0.0.1",
+   "main": "app.js",
+   "aliases": {
+      "@root": ".",
+      "@config": "./config.js",
+      "@routes": "routes/",
+      "@util": "modules/util"
+   },
+   "dependencies": {
+      
+   }
+}
+```
---
+++ aliasmo.js
@@ -0,0 +1,41 @@
+/*!
+ * Aliasmo, http://tpkn.me/
+ */
+const fs = require('fs');
+const path = require('path');
+const Module = require('module');
+const Require = Module.prototype.require;
+
+module.exports = (aliases_list) => {
+   try {
+
+      let aliases;
+      const package_json = path.join(path.dirname(require.main.filename), './package.json');
+
+      if(aliases_list instanceof Object){
+         aliases = aliases_list;
+      }else{
+         if(fs.existsSync(package_json)){
+            aliases = require(package_json).aliases;
+         }else{
+            return
+         }
+      }
+
+      Module.prototype.require = function(...args){
+         if(args[0].indexOf('@') === 0){
+            for(let alias in aliases){
+               if(args[0].indexOf(alias) !== -1){
+                  args[0] = path.resolve(args[0].replace(alias, aliases[alias]));
+                  break
+               }
+            }
+         }
+
+         return Require.apply(this, args);
+      }
+      
+   }catch(err){
+      console.log(err);
+   }
+}
